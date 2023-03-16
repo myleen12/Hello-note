3e94cfbe8d6c24e4e88aa7b00378f90b1dc36645
myleen12 <myleen.santana12@icloud.com>
2023-03-15 19:44:51 -0700

    created Html route

---
+++ Html-routes.js
@@ -0,0 +1,12 @@
+const path = require('path');
+ 
+module.exports = function(app) {
+
+    app.get('/note', function(req, res) {
+        res.sendFile(path.json(__dirname, "../public/assests/notes.html"));
+    });
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname, "../pulblic/assests/index.html"));
+    
+    });
+}
