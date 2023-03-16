6cd71e4e88fc8d0e337bff0ca26215fb6ca94694
myleen12 <myleen.santana12@icloud.com>
2023-03-15 19:45:44 -0700

    undated path.join typo

--- Html-routes.js
+++ Html-routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function(app) {
 
     app.get('/note', function(req, res) {
-        res.sendFile(path.json(__dirname, "../public/assests/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/assests/notes.html"));
     });
         app.get('*', function(req, res) {
             res.sendFile(path.join(__dirname, "../pulblic/assests/index.html"));
