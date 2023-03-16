dc4385c063d77220367a8a176cbb054ff6b1164d
myleen12 <myleen.santana12@icloud.com>
2023-03-15 20:12:44 -0700

    created API route

---
+++ API-route.js
@@ -0,0 +1,12 @@
+const path = reqire('path');
+
+module.exports = function(app) {
+    
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/assests/index.html'));
+    });
+
+    app.get('./note', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/assests/note.html'));
+    });
+}
